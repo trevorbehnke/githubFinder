6442ff767f4e472d24add8d929de1df7bf5bcd33
trevor <me@trevorbehnke.com>
2021-07-23 22:16:50 -0700

    + alert message && find repos

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-// Initialize GitHub
+// Init Github
 const github = new Github();
+// Init UI
+const ui = new UI();
 
 // Search input
 const searchUser = document.getElementById("searchUser");
@@ -14,9 +16,15 @@ searchUser.addEventListener("keyup", (e) => {
     github.getUser(userText).then((data) => {
       if (data.profile.message === "Not Found") {
         // Show alert
+        ui.showAlert("User not found", "alert alert-danger");
       } else {
         // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
       }
     });
+  } else {
+    // Clear profile
+    ui.clearProfile();
   }
 });
--- github.js
+++ github.js
@@ -1,7 +1,9 @@
 class Github {
   constructor() {
     this.client_id = "1071af8b914a49f2baf9";
-    this.client_secret = "32728643a996088297d713d5f09dce35b5e68a13";
+    this.client_secret = "c8ec86107fa41b0620d3abc8208d38a54e9478e5";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -9,10 +11,16 @@ class Github {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
       profile,
+      repos,
     };
   }
 }
